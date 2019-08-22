1a0384a047a24512da8d67ac402222f93c618dc7
leeroywking <leeroywking@gmail.com>
2019-08-21 19:54:41 -0700

    tested the /launch route

--- src/routes/launch.js
+++ src/routes/launch.js
@@ -4,12 +4,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../auth/middleware.js');
 
-router.get('/launch', auth, theMagic);
+router.post('/launch', auth, theMagic);
 
 // Route Handlers
 function theMagic(req, res, next) {
   let thingie = req.body
   let output = thingie;
+  console.log(thingie)
   res.status(200).json(output);
 }
 
