f7d553dfa8a550acaa4bb72d1362b7573c1b3c80
leeroywking <leeroywking@gmail.com>
2019-08-26 13:36:18 -0700

    update

--- src/testingshellscript.js
+++ src/testingshellscript.js
@@ -11,7 +11,7 @@ let inputObj = {
     env: ``
 }
 
-inputObj2 = {
+let inputObj2 = {
     repo:'https://github.com/leeroywking/andy-best-frand.git',
     repoName: 'andy-best-frand',
     entryPoint: `index.js`,
@@ -24,7 +24,7 @@ shellscript(inputObj,inUsePortPool).then(res => console.log(res))
 shellscript(inputObj2, inUsePortPool).then(res => console.log(res))
 
 /*
-echo '{"repo": "https://github.com/leeroywking/bad-guy-quotes.git","repoName": "bad-guy-quotes","entryPoint":"server.js","env": ""}' | http POST :3000/launch -a lee:lee
+echo '{"repo": "https://github.com/leeroywking/bad-guy-quotes.git","repoName": "bad-guy-quotes","entryPoint":"server.js","env": ""}' | http POST ec2-34-211-96-238.us-west-2.compute.amazonaws.com:3000/launch -a lee:lee
 
 echo '{"repo": "https://github.com/leeroywking/andy-best-frand.git","repoName": "andy-best-frand","entryPoint": "index.js","env": ""}' | http POST :3000/launch -a lee:lee
 */
